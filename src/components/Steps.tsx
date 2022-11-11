38488f89b8874d097e721e935981ce979be6942d
Julian Haeger <julian.haeger@evergreensmartpower.co.uk>
2022-11-11 19:41:24 +0000

    added prop to HiddenStep to have it reveal when another step is revealed

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -5,7 +5,7 @@ import { useJourneyState } from '../library';
 // const hideWhileFuture = (id: string): React.CSSProperties => props.journeyState.isFuture(id) ? {transition: 'opacity ease 1s', opacity: 0} : {transition: 'opacity easy 1s'}
 type HiddenStepProps = {
   id?: string,
-  triggerId?: string,
+  displayWith?: string,
   style?: CSSProperties
   styleWhileFuture?: CSSProperties
   wrapper?: 'div' | 'li' | 'span'
@@ -14,10 +14,13 @@ type HiddenStepProps = {
 export const HiddenStep: React.FunctionComponent<PropsWithChildren<HiddenStepProps>> = (props) => {
   const journeyState = useJourneyState()
   const id = props.id ?? `step-${useId()}`
-  const isFuture = journeyState.isFuture(props.triggerId ?? id)
+  const isFuture = journeyState.isFuture(props.displayWith ?? id)
   const hiddenStyle = {opacity: 0, ...(props.styleWhileFuture ?? {})}
   const conditionalStyle: CSSProperties = isFuture ? hiddenStyle : {}
   const style: CSSProperties = {transition: 'opacity ease 1s', ...(props.style ?? {}), ...conditionalStyle}
 
-  return createElement(props.wrapper ?? 'div', {style, className:'journey-step', id, children: props.children})
+  // We don't want to be a click-stop if we are piggybacking off another step being revealed
+  const classNameMixin = props.displayWith === undefined ? {className: 'journey-step'} : {}
+
+  return createElement(props.wrapper ?? 'div', {style, ...classNameMixin, id, children: props.children})
 }
