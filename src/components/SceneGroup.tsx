fee642fdb8eca6eb33c71aa028258ff4c15aac47
Julian Haeger <julian.haeger@evergreensmartpower.co.uk>
2022-11-15 08:53:27 +0000

    Scenes no longer default to absolute positioning if no position is passed in

--- src/components/SceneGroup.tsx
+++ src/components/SceneGroup.tsx
@@ -8,10 +8,16 @@ export type Props = {
   scale?: number
 }
 export const SceneGroup: React.FC<Props> = (props) => {
+  const isAbsolutelyPositioned = props.position !== undefined
+
   const scalePrefix = props.scale ? `scale(${props.scale}, ${props.scale}) ` : ''
-  const transform = {transform: scalePrefix + 'translate(-50%, -50%) translateZ(0)'}
-  // const transform = {transform: 'translate(-50%, -50%) translateZ(0)'}
-  const style: React.CSSProperties = {position: 'relative', overflow: 'visible', ...transform, ...generatePositionStyle(props), ...props.style}
+  const centreTransform = isAbsolutelyPositioned ? 'translate(-50%, -50%)' : ''
+  const zTransform = 'translateZ(0)'
+  const transform = {transform: [scalePrefix, centreTransform, zTransform].join(' ')}
+
+  const positionMixin: React.CSSProperties = isAbsolutelyPositioned ? {position: 'absolute'} : {}
+
+  const style: React.CSSProperties = {...positionMixin, overflow: 'visible', ...transform, ...generatePositionStyle(props), ...props.style}
   return (
     <div className='scene-group' style={style}>
       {props.children}
